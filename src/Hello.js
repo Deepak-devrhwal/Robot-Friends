6cec55b700187d6a60673b2e6d1ecdc807af5fb6
Deepak Devrhwal <deepakdevrhwal@gmail.com>
2020-05-05 01:48:15 +0530

    updates

---
+++ src/Hello.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./hell.css";
+class Hello extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello world</h1>
+        <p>welcome</p>
+      </div>
+    );
+  }
+}
+export default Hello;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+// import CardList from "./CardList";
 import * as serviceWorker from "./serviceWorker";
+import "tachyons";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
