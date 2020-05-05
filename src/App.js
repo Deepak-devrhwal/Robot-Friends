960b5a4c58ec432bd8b73ef63586761c24f4a24d
Deepak Devrhwal <deepakdevrhwal@gmail.com>
2020-05-05 11:50:14 +0530

    contd.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
 import React from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
+import "./App.css";
 
-const App = () => {
-  return (
-    <div className='tc'>
-      <h1>RoboFriends</h1>
-      <SearchBox />
-      <CardList />
-    </div>
-  );
-};
+// import { render } from "react-dom";
+import { robots } from "./robots";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: robots,
+      searchfield: "",
+    };
+  }
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value });
+
+    // console.log(xp);
+  };
+  render() {
+    const xp = this.state.robots.filter((robots) => {
+      return robots.name
+        .toLowerCase()
+        .includes(this.state.searchfield.toLowerCase());
+    });
+    return (
+      <div className='tc'>
+        <h1 className='f1'>RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <CardList robots={xp} />
+      </div>
+    );
+  }
+}
 export default App;
